c98d98253a9cb553f9f799f4568f9bec2aac9f69
Pasquale Arieta <Pasquale Arieta>
2015-12-12 13:50:02 +0100

    new Chrono

---
+++ PefSPool/src/igpe/billiard/core/Chronometer.java
@@ -0,0 +1,55 @@
+package igpe.billiard.core;
+
+public class Chronometer {
+
+	private long elapsedTime = 0;
+	private long startTime;
+	private boolean isRunning;
+	private long endTime;
+
+	public Chronometer() {
+		reset();
+	}
+
+	public void start() {
+		if (!isRunning) {
+			isRunning = true;
+			startTime = System.currentTimeMillis();
+		}
+	}
+
+	public void stop() {
+		if (isRunning) {
+			isRunning = false;
+			endTime = System.currentTimeMillis();
+			elapsedTime = elapsedTime + endTime - startTime;
+		}
+	}
+
+	public long getElapsedTime() {
+		if (isRunning) {
+			endTime = System.currentTimeMillis();
+			elapsedTime = elapsedTime + endTime - startTime;
+			startTime = endTime;
+		}
+		return elapsedTime / 1000;
+	}
+
+	public void reset() {
+		elapsedTime = 0;
+		isRunning = false;
+	}
+
+	public void resume(long b) {
+		long value=b;
+		value = value * 1000;
+		elapsedTime = 0;
+		startTime = System.currentTimeMillis() -value;
+
+	}
+
+	public long getMinute() {
+		return getElapsedTime() / 60;
+	}
+
+}
